f76054129267f962eaf523c3ecdb5d295f577c18
Rene Bremer <rebremer@microsoft.com>
2020-10-26 21:35:47 +0100

    change order of adding firewall rules

--- scripts/6_configure_firewall.sh
+++ scripts/6_configure_firewall.sh
@@ -5,24 +5,27 @@ fi
 #
 # 0. Service endpoints
 az network vnet subnet update --resource-group $RG --vnet-name $VNET --name "public-subnet" --service-endpoints "Microsoft.KeyVault" "Microsoft.Storage" "Microsoft.AzureCosmosDB"
-# 1. Key vault
-az keyvault update --resource-group "$RG" --name "$AKV" --default-action Deny
-# Databricks
 cli_response=$(az network vnet subnet show --resource-group "$RG" --vnet-name "$VNET" --name "public-subnet")
 subnet_id=$(jq .id -r <<< "$cli_response")
+#
+# 1. Cosmos DB
+#az cosmosdb account update --resource-group "$RG" --name "$COSMOSDBNAME" --enable-public-network false
+# Databricks
+az cosmosdb update -g $RG -n $COSMOSDBNAME --enable-virtual-network true
+az cosmosdb network-rule add -g $RG -n $COSMOSDBNAME --subnet $subnet_id
+#
+# 2. Key vault
+az keyvault update --resource-group "$RG" --name "$AKV" --default-action Deny
+# Databricks
 az keyvault network-rule add -g "$RG" -n "$AKV" --subnet $subnet_id
 # Azure Data Factory
 az keyvault update --resource-group "$RG" --name "$AKV" --bypass AzureServices
 #
-# 2. Storage account
+# 3. Storage account
 az storage account update --resource-group "$RG" --name "$STOR" --default-action Deny
 # Databricks
 az storage account network-rule add -g $RG --account-name $STOR --subnet $subnet_id
 # Azure Data Factory
 az storage account update -g "$RG" -n "$STOR" --bypass AzureServices
 #
-# 2. Cosmos DB
-#az cosmosdb account update --resource-group "$RG" --name "$COSMOSDBNAME" --enable-public-network false
-# Databricks
-az cosmosdb update -g $RG -n $COSMOSDBNAME --enable-virtual-network true
-az cosmosdb network-rule add -g $RG -n $COSMOSDBNAME --subnet $subnet_id
+sleep 1m # avoid raise conditions
