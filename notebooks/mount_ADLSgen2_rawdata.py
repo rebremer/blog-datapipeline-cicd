eabe279ed860a9cce504d67a865245b7878504e7
Rene Bremer <rebremer@microsoft.com>
2020-11-12 20:07:09 +0100

    use object id of SPN for RBAC on storage account

--- notebooks/mount_ADLSgen2_rawdata.py
+++ notebooks/mount_ADLSgen2_rawdata.py
@@ -19,7 +19,7 @@ configs = {"fs.azure.account.auth.type": "OAuth",
            "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
            "fs.azure.account.oauth2.client.id": dbutils.secrets.get(scope="dbrkeys",key="spn-id"),
            "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope="dbrkeys",key="spn-key"),
-           "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/" + dbutils.secrets.get(scope="dbrkeys",key="tenand-id") + "/oauth2/token"}
+           "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/" + dbutils.secrets.get(scope="dbrkeys",key="tenant-id") + "/oauth2/token"}
 
 # Optionally, you can add <directory-name> to the source URI of your mount point.
 dbutils.fs.mount(
@@ -27,14 +27,9 @@ dbutils.fs.mount(
   mount_point = "/mnt/defineddata",
   extra_configs = configs)
 
-dbutils.fs.mount(
-  source = "abfss://libraries@" + par_stor_name + ".dfs.core.windows.net/",
-  mount_point = "/mnt/libraries",
-  extra_configs = configs)
-
 # COMMAND ----------
 
 %sh
-ls -l /dbfs/mnt/libraries
+ls -l /dbfs/mnt/defineddata
 
 # COMMAND ----------
--- scripts/3_configure_access_storage_databricks.sh
+++ scripts/3_configure_access_storage_databricks.sh
@@ -4,7 +4,7 @@ if [ $ACCESS_STOR_AADDBR = 0 ]; then
     stor_key=$(jq .[0].value -r <<< "$key_response")
     az keyvault secret set -n stor-key --vault-name $AKV --value $stor_key
 else
-    echo "Assging Azure AD SPN to Databricks for authentication to storage account"
+    echo "Assigning Azure AD SPN to Databricks for authentication to storage account"
     spn_response=$(az ad sp create-for-rbac -n $SPN --skip-assignment)
     spn_id=$(jq .appId -r <<< "$spn_response")
     spn_key=$(jq .password -r <<< "$spn_response")
@@ -12,8 +12,10 @@ else
     az keyvault secret set -n spn-id --vault-name $AKV --value $spn_id
     az keyvault secret set -n spn-key --vault-name $AKV --value $spn_key
     #
+    spn_response=$(az ad sp show --id $spn_id)
+    spn_object_id=$(jq .objectId -r <<< "$spn_response")
     scope="/subscriptions/$SUB/resourceGroups/$RG/providers/Microsoft.Storage/storageAccounts/$STOR"
-    az role assignment create --assignee-object-id $spn_id --role "Storage Blob Data Contributor" --scope $scope
+    az role assignment create --assignee-object-id $spn_object_id --role "Storage Blob Data Contributor" --scope $scope
     #
     # In case a Databricks secret scope is used and this script is run afterwards with elevated user rights,
     # also run script 4_configure_secret_scope_databricks.sh to add spn_id and spn_key to Databricks secret scope
